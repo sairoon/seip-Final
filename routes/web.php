0cf0027a9f035d33cb5c62e20262c9b2195c11eb
S. M Khaled Mahmud <smbdmahmud1010@gmail.com>
2024-06-05 02:23:36 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,16 +6,6 @@ use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\ProductController;
 use  App\Http\Controllers\Admin\CategoryController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', [HomeController::class, 'home'])->name('home');
 Route::get('/category-products', [HomeController::class, 'category'])->name('categories');
